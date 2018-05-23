64842ec4c4204274f58579160d7da3fea9685e47
Jorge Ruiz <jorge1797@gmail.com>
2018-05-23 17:04:21 -0500

    Agenda api passport routes

--- routes/api.php
+++ routes/api.php
@@ -23,9 +23,14 @@ Route::group(['prefix' => '/v1', 'namespace' => 'Api\V1', 'as' => 'api.'], funct
     Route::resource('categories', 'CategoriesController', ['except' => ['create', 'edit', 'update']]);
 });
 
-Route::post('login', 'API\UserController@login');
-Route::post('register', 'API\UserController@register');
+Route::group(['prefix' => '/external', 'middleware' => 'auth:api', 'namespace' => 'Api\V1', 'as' => 'api.external.'], function () {
+    Route::resource('reminders', 'RemindersController', ['except' => ['create', 'edit']]);
+    Route::resource('archive_reminders', 'ArchiveRemindersController', ['except' => ['create', 'edit', 'update']]);
+    Route::resource('categories', 'CategoriesController', ['except' => ['create', 'edit', 'update']]);
+});
 
-Route::group(['middleware' => 'auth:api'], function(){
-    Route::post('details', 'API\UserController@details');
+Route::group(['prefix' => '/external', 'namespace' => 'Api'], function () {
+    Route::middleware('auth:api')->get('details', 'UserController@details');
+    Route::post('login', 'UserController@login');
+    Route::post('register', 'UserController@register');
 });
