72160c179d37677a7e75872955aba0f45212e17b
Jorge Ruiz <jorge1797@gmail.com>
2018-06-12 16:09:35 -0500

    External API changes

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,12 +5,16 @@ namespace App\Http\Controllers\API;
 use Illuminate\Http\Request; 
 use App\Http\Controllers\Controller; 
 use App\User; 
+use App\VerifyUser;
+use App\Mail\VerifyMail;
 use Illuminate\Support\Facades\Auth; 
 use Validator;
+use Mail;
 
 class UserController extends Controller {
 
     public $successStatus = 200;
+    
     /** 
      * login api 
      * 
@@ -19,13 +23,19 @@ class UserController extends Controller {
     public function login(){ 
         if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){ 
             $user = Auth::user(); 
-            $success['token'] =  $user->createToken('MyApp')-> accessToken; 
-            return response()->json(['success' => $success], $this-> successStatus); 
+
+            if ($user->verified) {
+                $success['token'] =  $user->createToken('MyApp')-> accessToken; 
+                return response()->json(['success' => $success], $this-> successStatus); 
+            } else {
+                return response()->json(['warning' => 'You need to confirm your account. We have sent you an activation code, please check your email.'], $this-> successStatus);
+            }
         } 
         else{ 
-            return response()->json(['error'=>'Unauthorised'], 401); 
+            return response()->json(['error'=>'Unauthorized'], 401); 
         } 
     }
+
     /** 
      * Register api 
      * 
@@ -47,11 +57,20 @@ class UserController extends Controller {
         $input = $request->all(); 
         $input['password'] = bcrypt($input['password']); 
         $user = User::create($input); 
-        $success['token'] =  $user->createToken('MyApp')-> accessToken; 
-        $success['name'] =  $user->name;
 
-        return response()->json(['success'=>$success], $this-> successStatus); 
+        $verifyUser = VerifyUser::create([
+            'user_id' => $user->id,
+            'token' => str_random(40)
+        ]);
+
+        //$success['token'] =  $user->createToken('MyApp')-> accessToken; 
+        //$success['name'] =  $user->name;
+
+        Mail::to($user->email)->send(new VerifyMail($user));
+
+        return response()->json(['success'=>'We sent you an activation code. Check your email and click on the link to verify.'], $this-> successStatus); 
     }
+
     /** 
      * details api 
      * 
